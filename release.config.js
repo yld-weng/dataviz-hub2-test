46462cc60c5304437ce20031c361ac7f535560a8
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2022-01-13 20:06:36 +0000

    fix: change release file

---
+++ release.config.js
@@ -0,0 +1,142 @@
+module.exports = 
+{
+  "defaultBranch": "main",
+  "branches": [
+    "+([0-9])?(.{+([0-9]),x}).x", 
+    "main", 
+    "next", 
+    "next-major", 
+    { "name": "beta", "prerelease": true }, 
+    { "name": "alpha", "prerelease": true }
+  ],
+  "plugins": [
+    ["@semantic-release/commit-analyzer", {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "feat", "release": "minor"},
+        {"type": "fix", "release": "patch"},
+        {"type": "perf", "release": "patch"},
+        {"type": "docs", "scope": "README", "release": "patch"},
+        {"type": "docs", "scope": "new-*", "release": "minor"},
+        {"type": "refactor", "scope": "core-*", "release": "minor"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "style", "release": "patch"},
+        {"type": "test", "release": "patch"},
+        {"type": "chore", "release": "patch"},
+        {"breaking": true, "release": "major"},
+        {"revert": true, "release": "patch"},
+        {"scope": "no-release", "release": false}
+      ],
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
+    "@semantic-release/release-notes-generator",
+    [ 
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.mdx"
+      } 
+    ],
+    [
+      "@semantic-release/npm", 
+      {
+        "npmPublish": false
+      }
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "CHANGELOG.mdx",
+          "package.json",
+          "package-lock.json",
+          "npm-shrinkwrap.json"
+        ]
+      }
+    ]
+  ],
+  "generateNotes": {
+    "preset": "angular",
+    "writeOpts": {
+      "transform": (commit, context) => {
+        const issues = []
+
+        commit.notes.forEach(note => {
+            note.title = `BREAKING CHANGES`
+        })
+
+        // NOTE: Any changes here must be reflected in `CONTRIBUTING.md`.
+        if (commit.type === `feat`) {
+            commit.type = `Features`
+        } else if (commit.type === `fix`) {
+            commit.type = `Bug Fixes`
+        } else if (commit.type === `perf`) {
+            commit.type = `Performance Improvements`
+        } else if (commit.type === `revert`) {
+            commit.type = `Reverts`
+        } else if (commit.type === `docs`) {
+            commit.type = `Documentation`
+        } else if (commit.type === `style`) {
+            commit.type = `Styles`
+        } else if (commit.type === `refactor`) {
+            commit.type = `Code Refactoring`
+        } else if (commit.type === `test`) {
+            commit.type = `Tests`
+        } else if (commit.type === `build`) {
+            commit.type = `Build System`
+        // } else if (commit.type === `chore`) {
+        //     commit.type = `Maintenance`
+        } else if (commit.type === `ci`) {
+            commit.type = `Continuous Integration`
+        } else {
+            return
+        }
+
+        if (commit.scope === `*`) {
+            commit.scope = ``
+        }
+
+        if (typeof commit.hash === `string`) {
+            commit.shortHash = commit.hash.substring(0, 7)
+        }
+
+        if (typeof commit.subject === `string`) {
+            let url = context.repository
+                ? `${context.host}/${context.owner}/${context.repository}`
+                : context.repoUrl
+            if (url) {
+                url = `${url}/issues/`
+                // Issue URLs.
+                commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+                    issues.push(issue)
+                    return `[#${issue}](${url}${issue})`
+                })
+            }
+            if (context.host) {
+                // User URLs.
+                commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+                if (username.includes('/')) {
+                    return `@${username}`
+                }
+
+                return `[@${username}](${context.host}/${username})`
+                })
+            }
+        }
+
+        // remove references that already appear in the subject
+        commit.references = commit.references.filter(reference => {
+            if (issues.indexOf(reference.issue) === -1) {
+                return true
+            }
+
+            return false
+        })
+
+        return commit
+      },
+    }
+  }
+}
