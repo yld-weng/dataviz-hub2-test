1d230627a49fdb742a2580db28e499ba29636e3c
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2022-01-17 12:24:03 +0000

    build: add preset config

--- release.config.js
+++ release.config.js
@@ -21,7 +21,59 @@ module.exports = {
     ],
     ["@semantic-release/release-notes-generator", {
       "preset": "conventionalcommits",
-      "presetConfig": "./semantic-release-config/conventionalCommits-preset.js"
+      "presetConfig": {
+        "types": [
+          {
+            "type": "feat",
+            "section": ":sparkles: Features",
+            "hidden": false
+          },
+          {
+            "type": "fix",
+            "section": ":bug: Bug Fixes",
+            "hidden": false
+          },
+          {
+            "type": "docs",
+            "section": ":memo: Documentation",
+            "hidden": false
+          },
+          {
+            "type": "style",
+            "section": ":art: Styles",
+            "hidden": false
+          },
+          {
+            "type": "refactor",
+            "section": ":recycle: Code Refactoring",
+            "hidden": false
+          },
+          {
+            "type": "perf",
+            "section": ":zap: Performance Improvement",
+            "hidden": false
+          },
+          {
+            "type": "test",
+            "section": ":white_check_mark: Testing",
+            "hidden": false
+          },
+          {
+            "type": "build",
+            "section": ":hammer: Build/Dependencies",
+            "hidden": false
+          },
+          {
+            "type": "ci",
+            "section": ":wrench: Continuous Integration",
+            "hidden": false
+          },
+          {
+            "type": "chore",
+            "hidden": true
+          }
+        ]
+      }
     }],
     [
       "@semantic-release/changelog", 
